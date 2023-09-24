d86b894f670f9b90cc0139c17aa398d2736732db
shgi99 <shgi7694@gmail.com>
2023-09-25 02:36:50 +0900

    230925

--- UnityChan_UGUI/Assets/Scripts/MsgDisp.cs
+++ UnityChan_UGUI/Assets/Scripts/MsgDisp.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MsgDisp : MonoBehaviour
 {
+    public static string msg;
+    public static bool flagDisplay;
+
+    public Image Msg;
+    public Text msgtext;
+    private float waitDelay;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,40 @@ public class MsgDisp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (flagDisplay)
+        {
+            waitDelay += Time.deltaTime;
+            
+            if(waitDelay > 6.0f)
+            {
+                flagDisplay = false;
+                waitDelay = 0;
+            }
+            else
+            {
+                Msg.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Msg.gameObject.SetActive(false);
+        }
+
+        if (!string.IsNullOrEmpty(msg))
+        {
+            ShowMessage(msg);
+            msg = null;
+        }
+           
+    }
+    public static void SaveMessage(string msg)
+    {
+        MsgDisp.msg = msg;
+    }
+
+    public void ShowMessage(string msg)
+    {
+        flagDisplay = true;
+        msgtext.text = msg;
     }
 }
---
+++ UnityChan_UGUI/Assets/Scripts/Touch_.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch_ : MonoBehaviour
+{
+    System.DateTime now;
+    public AudioClip voice1;
+    public AudioClip voice2;
+    private Animator animator;
+    private AudioSource univoice;
+
+    // 모션 스테이트의 ID 얻기
+    private int motionIdol = Animator.StringToHash("Base Layer.Idol");
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        univoice = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        now = System.DateTime.Now;
+        // false로 설정해놓아야 클릭했을때만 true로 전환함으로써 애니메이션이 발동되도록 가능
+        animator.SetBool("Touch", false); 
+        animator.SetBool("TouchHead", false);
+
+        if(animator.GetCurrentAnimatorStateInfo(0).fullPathHash == motionIdol ) // 재생 중인 동작이 대기 동작인지 검사
+        {
+            animator.SetBool("Motion_Idle", true);
+        }
+        else
+        {
+            animator.SetBool("Motion_Idle", false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100)) 
+            {
+                GameObject hitObj = hit.collider.gameObject; // 터치한 부분의 오브젝트 파악 후 부위(태크)별로 반응 다르게 하기
+                if(hitObj.tag == "Head")
+                {
+                    animator.SetBool("TouchHead", true);
+                    animator.SetBool("Face_Happy", true);
+                    animator.SetBool("Face_Angry", false);
+                    univoice.clip = voice1;
+                    univoice.Play();
+                    MsgDisp.SaveMessage("안녕!\n오늘도 힘차게 시작해보자!");
+                }
+                else if(hitObj.tag == "Body")
+                {
+                    animator.SetBool("Touch", true);
+                    animator.SetBool("Face_Happy", false);
+                    animator.SetBool("Face_Angry", true);
+                    univoice.clip = voice2;
+                    univoice.Play();
+                    MsgDisp.SaveMessage("꺅!");
+                }
+                else if(hitObj.tag == "Arms")
+                {
+                    MsgDisp.SaveMessage("지금은\n" + now.Year + "년 " + now.Month + "월 " + now.Day + "일 " + now.Hour + "시 " + now.Minute + "분 " + now.Second + "초야!");
+                }
+            }
+        }
+    }
+}
